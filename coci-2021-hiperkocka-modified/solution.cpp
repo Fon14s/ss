4e6b1974246fe0fb153fe58b288ca868c4c3aeff
Fatih Naufal Habibillah <goldhunt2024@gmail.com>
2025-11-19 16:36:34 +0700

    Update solution.cpp

--- coci-2021-hiperkocka-modified/solution.cpp
+++ coci-2021-hiperkocka-modified/solution.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+#define For(i,a,b) for(int i=a;i<=b;i++)
+#define Rev(i,a,b) for(int i=a;i>=b;i--)
+using namespace std;
+constexpr int N = 55;
+int n, a[N], cnt;
+vector<int> to[N];
+void dfs(int u, int pa) {
+    for (int v : to[u])
+        if (v != pa)
+            a[v] = a[u] ^ (1 << (cnt++)), dfs(v, u);
+}
+int main() {
+    cin >> n;
+    For(i, 1, n) {
+        int x, y;
+        cin >> x >> y;
+        to[x].push_back(y);
+        to[y].push_back(x);
+    }
+    dfs(0, -1);
+    cout << (1 << (n - 1)) << '\n';
 
+    For(s, 0, (1 << n) - 1) if (__builtin_parity(s))
+        For(i, 0, n) cout << (s ^ a[i]) << " \n"[i == n];
+
+    return 0;
+}
